510359ffecb5c2c74e5fb48d4001dbe7ebd4d8e2
unknown <escorpio_89_32@hotmail.com>
2021-03-09 10:33:28 -0300

    mandar

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body>
+	<form action="#" method="POST">
+ <script
+ src="http://misenvios.com.ar/api/api.min.js"
+ data-publictoken="4A845D700D234FC8B1FF752DCFE5350482C76B6EC5BE478296"
+ data-transaction-id="10000" 
+ >
+ </script>
+
+</body>
+</html>
 <?php
 $api_keys = array(
 'apikey' => 'Ff1X1AgNgmQZsl4n9SdBoof7SRXrwA20xGXMMQDPioHXLVUPJX',
@@ -9,13 +27,18 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_POST, 1);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
 curl_setopt($curl, CURLOPT_URL,
-'https://api.misenvios.com.ar/api/Tokens');
+'https://misenvios.com.ar/servicios/api/Tokens');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array(
  'Content-Type: application/json',
  'Content-Length: ' . strlen($payload))
 );
 $curl_res = curl_exec($curl);
+
+echo $curl;
+echo $curl_res;
+ echo "<script>console.log('" .$curl_res. "');</script>";
+
 curl_close($curl);
 if( $curl_res ){
  $res = json_decode($curl_res, true);
@@ -24,3 +47,4 @@ if( isset($res["isError"]) && !$res["isError"])
 }
 echo "access-token : " . $access_token;
 ?>
+
