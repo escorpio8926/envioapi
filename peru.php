2d064e16727f466da1398d6cad85cfee2a42bfef
unknown <escorpio_89_32@hotmail.com>
2021-05-06 13:14:41 -0300

    testeando parte 3

--- peru.php
+++ peru.php
@@ -7,17 +7,26 @@
 </head>
 <body>
 <form action="#" method="POST">
-<script
- src="https://misenvios.com.ar/app/api/api.min.js"e
- 
+<script src="https://misenvios.com.ar/app/api/api.min.js"
+ data-origen-locationID="2021"
  data-public-token="ta8lmowoeUpZm5RASJPGfJHmXsBhouHIfCMbf0gPJ2wYDky9i0"
- 
- data-transaction-id="30021" 
- 
-
-
-></script>
+ data-callback="onCompleteShipping"
+ data-country="PE"
+ data-button-label="Realizar envio"
+ data-button-type="button" 
+ data-transaction-id="32321321111" 
+ data-transaction-contraentrega="true" 
+ data-transaction-autoconfirm="false" 
+ data-productos-descripcion="PAQUETE CON 4 iPHONE 12" 
+ data-productos-cantidad="1" 
+ data-productos-alto="100" 
+ data-productos-ancho="50" 
+ data-productos-profundidad="50" 
+ data-productos-peso="2" 
+ data-productos-tiempo="0"
+ ></script>
  </form>
+
 </body>
 </html>
 
