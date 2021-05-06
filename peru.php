b1749b3ada18b7d755437c9ca9301ef50dd396df
unknown <escorpio_89_32@hotmail.com>
2021-05-06 11:34:02 -0300

    testeando

--- peru.php
+++ peru.php
@@ -6,36 +6,6 @@
 	<title>Document</title>
 </head>
 <body>
-<?php
-$api_keys = array(
-'apikey' => 'Ff1X1AgNgmQZsl4n9SdBoof7SRXrwA20xGXMMQDPioHXLVUPJX',
-'secretkey' => 'E0QN0FAVAHE4CKZ8'
-);
-$payload = json_encode($api_keys);
-$access_token = null;
-$curl = curl_init();
-curl_setopt($curl, CURLOPT_POST, 1);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $payload);
-curl_setopt($curl, CURLOPT_URL,
-'https://misenvios.com.ar/servicios/api/Tokens');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($curl, CURLOPT_HTTPHEADER, array(
- 'Content-Type: application/json',
- 'Content-Length: ' . strlen($payload))
-);
-$curl_res = curl_exec($curl);
-
- echo "<script>console.log('" .$curl_res. "');</script>";
-
-curl_close($curl);
-if( $curl_res ){
- $res = json_decode($curl_res, true);
-if( isset($res["isError"]) && !$res["isError"])
- $access_token = $res["result"]["access_token"];
-}
-echo "access-token : " . $access_token;
-?>
-
 <form action="#" method="POST">
 <script src="https://misenvios.com.ar/app/api/api.min.js"
  data-origen-locationID="2021"
@@ -44,7 +14,7 @@ echo "access-token : " . $access_token;
  data-country="PE"
  data-button-label="Realizar envio"
  data-button-type="button" 
- data-transaction-id="10000" 
+ data-transaction-id="32321321111" 
  data-transaction-contraentrega="true" 
  data-transaction-autoconfirm="false" 
  data-productos-descripcion="PAQUETE CON 4 iPHONE 12" 
